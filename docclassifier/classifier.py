d897ee8f600133f678992b68fa446e35eef2bf5e
zephyr-sh <shaynling@gmail.com>
2024-01-20 13:25:43 +0800

    [C] Update infer modules

---
+++ docclassifier/classifier.py
@@ -0,0 +1,52 @@
+from enum import Enum
+from typing import Union
+
+import docsaidkit as D
+import numpy as np
+
+from .margin_based import Inference as MarginBasedInference
+
+__all__ = [
+    'DocClassifier', 'MarginBasedInference', 'ModelType'
+]
+
+
+class ModelType(D.EnumCheckMixin, Enum):
+    margin_based = 1
+
+
+class DocClassifier:
+
+    def __init__(
+        self,
+        model_type: ModelType = ModelType.margin_based,
+        model_cfg: str = None,
+        backend: D.Backend = D.Backend.cpu,
+        gpu_id: int = 0,
+        **kwargs
+    ):
+        model_type = ModelType.obj_to_enum(model_type)
+        if model_type == ModelType.margin_based:
+            model_cfg = 'lcnet050_arcface' if model_cfg is None else model_cfg
+            valid_model_cfgs = list(MarginBasedInference.configs.keys())
+            if model_cfg not in valid_model_cfgs:
+                raise ValueError(
+                    f'Invalid model_cfg: {model_cfg}, '
+                    f'valid model_cfgs: {valid_model_cfgs}'
+                )
+            self.classifier = MarginBasedInference(
+                gpu_id=gpu_id,
+                backend=backend,
+                model_cfg=model_cfg,
+                **kwargs
+            )
+
+    def list_models(self) -> list:
+        return list(self.classifier.configs.keys())
+
+    def __call__(self, img: np.ndarray,) -> Union[str, None]:
+        most_similar, max_score = self.classifier(img)
+        return most_similar, max_score
+
+    def __repr__(self) -> str:
+        return f'{self.classifier.__class__.__name__}({self.classifier.model})'
---
+++ docclassifier/margin_based/infer.py
@@ -0,0 +1,94 @@
+import os
+from typing import Tuple, Union
+
+import docsaidkit as D
+import numpy as np
+
+DIR = D.get_curdir(__file__)
+
+__all__ = ['Inference']
+
+
+def preprocess(
+    img: np.ndarray,
+    img_size_infer: Tuple[int, int] = None,
+    return_tensor: bool = True,
+):
+    if not D.is_numpy_img(img):
+        raise ValueError("Input image must be numpy array.")
+
+    if img_size_infer is not None:
+        img = D.imresize(img, size=img_size_infer)
+
+    if return_tensor:
+        img = np.transpose(img, axes=(2, 0, 1)).astype('float32')
+        img = img[None] / 255.
+
+    return {
+        'input': {'img': img},
+        'img_size_infer': img_size_infer,
+        'return_tensor': return_tensor,
+    }
+
+
+class Inference:
+
+    configs = {
+        'lcnet050_arcface': {
+            'model_path': 'lcnet050_arcface_fp32.onnx',
+            'file_id': '',
+            'img_size_infer': (96, 96),
+        },
+        'lcnet050_cosface': {
+            'model_path': 'lcnet050_cosface_fp32.onnx',
+            'file_id': '',
+            'img_size_infer': (96, 96),
+        },
+    }
+
+    def __init__(
+        self,
+        gpu_id: int = 0,
+        backend: D.Backend = D.Backend.cpu,
+        model_cfg: str = 'lcnet050_arcface',
+        threshold: float = 0.8,
+        docbank_root: Union[str, D.Path] = None,
+        **kwargs
+    ):
+        self.root = DIR / 'ckpt'
+        self.cfg = cfg = self.configs[model_cfg]
+        self.img_size_infer = cfg['img_size_infer']
+        model_path = str(self.root / cfg['model_path'])
+        if not D.Path(model_path).exists():
+            file_id = cfg['file_id']
+            os.system(D.gen_download_cmd(file_id, model_path))
+        self.model = D.ONNXEngine(model_path, gpu_id, backend, **kwargs)
+        self.bank = self.get_doc_bank(docbank_root)
+        self.threshold = threshold
+
+    @staticmethod
+    def compare(feat1, feat2):
+        return (np.dot(feat1, feat2) + 1) / 2
+
+    def get_doc_bank(self, root: Union[str, D.Path] = None):
+        root = DIR.parent / 'docbank' if root is None else D.Path(root)
+        return {
+            f.stem: self.extract_feature(D.imread(f))
+            for f in D.get_files(root, suffix=['.jpg', '.jpeg', '.png'])
+        }
+
+    def extract_feature(self, img: np.ndarray) -> np.ndarray:
+        img_infos = preprocess(img, img_size_infer=self.img_size_infer)
+        feats = self.model(**img_infos['input'])
+        return feats['feats'][0]
+
+    def __call__(self, img: np.ndarray) -> np.ndarray:
+        feats = self.extract_feature(img)
+        score_pool = []
+        for k, v in self.bank.items():
+            score = self.compare(feats, v)
+            if score > self.threshold:
+                score_pool.append((k, score))
+        most_similar, max_score = max(
+            score_pool, key=lambda x: x[1], default=(None, 0.))
+        return most_similar, max_score
