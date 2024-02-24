d194f2b0a221ca35bdf46195b58fd268c418a8a5
zephyr-sh <shaynling@gmail.com>
2024-02-24 10:02:19 +0800

    [F] Fixed default setting error

--- docclassifier/classifier.py
+++ docclassifier/classifier.py
@@ -30,7 +30,7 @@ class DocClassifier:
     ):
         model_type = ModelType.obj_to_enum(model_type)
         if model_type == ModelType.margin_based:
-            model_cfg = 'lcnet050_arcface' if model_cfg is None else model_cfg
+            model_cfg = 'lcnet050_cosface' if model_cfg is None else model_cfg
             valid_model_cfgs = list(MarginBasedInference.configs.keys())
             if model_cfg not in valid_model_cfgs:
                 raise ValueError(
